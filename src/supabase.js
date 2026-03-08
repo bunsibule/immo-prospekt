7b7c981ac7d88f15c47347c5608465e5eb27ac13
bunsibule <bonnet.mathieu@hotmail.fr>
2026-03-08 23:57:52 +0100

    Delete src directory

--- src/supabase.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(
-  'https://camrmyhxvocewfqxmevr.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhbXJteWh4dm9jZXdmcXhtZXZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwMDUzMDQsImV4cCI6MjA4ODU4MTMwNH0.WxjPl7yHSwF3DFHDdUoLTFkVQpqdeZ6fwT2HI0v-mzk'
-)
